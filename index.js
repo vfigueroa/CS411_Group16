e9e2a5690fc36edcc6fc97cb3ae275db88cebe54
Matt G <mgagnon1@live.com>
2020-11-07 12:52:22 -0500

    added make api call
    makes api call to refuge bathrooms and console logs nearby bathrooms based on current location in server

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const request = require('request');
+const fetch = require('node-fetch');
 const app = express();
 const path = require('path');
-
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-app.listen(PORT, () => console.log('Server started on port 3000'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.post('/getBathrooms' , (req, res) => {
+    var url = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&';
+    const data = req.body.lat;
+    var myReq = url + '&lat=' +  `${req.body.currentLat}` + '&' + '&lng=' +  `${req.body.currentLon}`;
+    console.log(myReq);
+    fetch(myReq)
+    .then(res => res.text())
+    .then(text => console.log(text))
+})
+
+
 
 
 
