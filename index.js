5ce5a0d2c8f6b9716036d24862b9d42afb5cc260
Matt G <mgagnon1@live.com>
2020-11-07 13:13:00 -0500

    Add files via upload
    removed app post call for api and moved the api call to client side for right now

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.post('/getBathrooms' , (req, res) => {
+/*
+app.get('/getBathrooms', (req,res) => {
     var url = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&';
     const data = req.body.lat;
     var myReq = url + '&lat=' +  `${req.body.currentLat}` + '&' + '&lng=' +  `${req.body.currentLon}`;
     console.log(myReq);
     fetch(myReq)
-    .then(res => res.text())
-    .then(text => console.log(text))
+    .then(response => res.text(response))
+    .then(text => console.log(text));
 })
-
+*/
 
 
 
