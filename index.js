2a0e263328fd826f651579953438ffbecd905db2
Matt G <mgagnon1@live.com>
2020-11-08 14:50:08 -0500

    Add files via upload
    made api call to refuge bathrooms server-side

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const app = express();
 const path = require('path');
+const https = require('https');
+const { resolveSoa } = require('dns');
 const PORT = process.env.PORT || 3000
 
+const app = express();
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-/*
-app.get('/getBathrooms', (req,res) => {
+
+
+
+app.get('/nearbyBathrooms:currentLat/:currentLon', async (request, response) => {
     var url = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&';
-    const data = req.body.lat;
-    var myReq = url + '&lat=' +  `${req.body.currentLat}` + '&' + '&lng=' +  `${req.body.currentLon}`;
-    console.log(myReq);
-    fetch(myReq)
-    .then(response => res.text(response))
-    .then(text => console.log(text));
+    var data = request.params;
+    var myReq = url + '&lat=' +  `${data.currentLat}` + '&' + '&lng=' +  `${data.currentLon}`;
+
+    const myrequest = await fetch(myReq);
+    const test = await myrequest.json();
+    response.send(test);
 })
-*/
+
 
 
 
