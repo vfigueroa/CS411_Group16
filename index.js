8c40252d0fd0b5094be672f01ee80f4e844fcdee
Matt G <mgagnon1@live.com>
2020-12-10 17:51:46 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -11,8 +11,22 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/landingPage.html'))
+})
 
-
+app.get('/searchBathrooms' , (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/searchBathrooms.html'))
+})
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/login.html'))
+})
+app.get('/quiz', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/quiz.html'))
+})
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/register.html'))
+})
 
 app.get('/nearbyBathrooms:currentLat/:currentLon', async (request, response) => {
     var url = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&';
