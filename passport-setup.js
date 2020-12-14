b742addaad9d632d08b164a8bf2a452efc3807b4
Matt G <mgagnon1@live.com>
2020-12-14 16:29:04 -0500

    Add files via upload

--- passport-setup.js
+++ passport-setup.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const passport = require('passport');
 const fetch = require('node-fetch');
 const express = require('express');
@@ -25,4 +26,33 @@ function(accessToken, refreshToken, profile, cb) {
   //console.log(accessToken, profile._json.given_name);
   
 }
+=======
+const passport = require('passport');
+const fetch = require('node-fetch');
+const express = require('express');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const config = require('./Config/config');
+const main = require('./index');
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+})
+
+passport.use(new GoogleStrategy({
+  clientID: config.googleConfig.CLIENT_ID,
+  clientSecret: config.googleConfig.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/google/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  // Register user here.
+  
+  cb(null, profile);
+    
+  //console.log(accessToken, profile._json.given_name);
+  
+}
+>>>>>>> 63422ead67502d4166a372ca6f7fdd43762704bb
 ));
